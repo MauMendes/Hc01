0dd05e7f173efef3464f45af3db1defdc3373f68
vini22correagit <vinicius@hardcode.com.br>
2020-09-08 10:39:47 -0300

    HC01 BLINK

--- HC01_BLINK/HC01_BLINK.ino
+++ HC01_BLINK/HC01_BLINK.ino
@@ -1,10 +1,7 @@
 /*
-  ESP8266 Blink by Simon Peter
-  Blink the blue LED on the ESP-01 module
+  HC-01 Blink
+  Blink the blue LED on the HC-01 module
   This example code is in the public domain
-
-  The blue LED on the ESP-01 module is connected to GPIO1
-  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
 
   Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
 */
@@ -20,5 +17,5 @@ void loop() {
   // it is active low on the ESP-01)
   delay(1000);                      // Wait for a second
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
+  delay(1000);                      // Wait for a second (to demonstrate the active low LED)
 }
